b917b74c18bfed14b75c831c724495192048ad81
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-10-07 19:38:01 +0600

    added home page and navbar with proper design

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,33 @@
+import Navbar from "../Navbar/Navbar";
+import banner from '../../../public/banner2.jpg'
+import { useLoaderData } from "react-router-dom";
+import Services from "../Services/Services";
+
+const Home = () => {
+    const services = useLoaderData();
+    return (
+        <div>
+            <div className="relative">
+                <img className="lg:h-[800px] w-full" src={banner} alt="" />
+                <div className="absolute top-0 w-full h-screen">
+                    <Navbar></Navbar>
+                    <div className="flex flex-col items-center md:mt-48 space-y-4 text-violet-700">
+                        <h1 className="text-2xl md:text-5xl lg:text-6xl font-bold">Perfect Planners for</h1>
+                        <h1 className="text-2xl md:text-5xl lg:text-6xl font-bold">Your Special Wedding Day</h1>
+                        <button className="btn btn-outline btn-primary font-bold">Enquire Now</button>
+                    </div>
+                </div>
+                    <div className="max-w-6xl mx-auto mt-8">
+                        <h1 className="text-4xl text-center font-bold mb-10">Our Services</h1>
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                            {
+                                services.map(service => <Services key={service._id} service={service}></Services>)
+                            }
+                        </div>
+                    </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+
+const Navbar = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleSignOut = () => {
+        logOut()
+            .then()
+            .catch()
+    }
+
+    const navLinks = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/register">Register</NavLink></li>
+    </>
+    return (
+        <div className="navbar">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {
+                            navLinks
+                        }
+                    </ul>
+                </div>
+                <a className="btn btn-ghost normal-case text-xl bg-violet-600 text-white">W-Planner</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {
+                        navLinks
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {
+                    user ? <p className="mr-2 font-bold">{user.displayName}</p> : ""
+                }
+                {
+                    user ? <img className="w-14 h-14 rounded-full mr-1" src={user.photoURL} alt="" /> : <img src="" alt="" />
+                }
+                {
+                    user ?
+                        <button className="btn hover:bg-violet-800 bg-violet-600 border-none font-bold text-white" onClick={handleSignOut}>Sign Out</button> :
+                        <Link to="/login">
+                            <button className="btn hover:bg-violet-800 bg-violet-600 border-none font-bold text-white">Login</button>
+                        </Link>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
